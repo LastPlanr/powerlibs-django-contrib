d6d8883c9d306da54b49cd3775c018540eb63c66
Renato Neto <30633715+neratneto@users.noreply.github.com>
2017-10-02 15:03:41 -0300

    Fixing indentation errors from flake8

--- powerlibs/django/contrib/related_objects.py
+++ powerlibs/django/contrib/related_objects.py
@@ -2,7 +2,7 @@ from django.db.models.deletion import Collector
 
 
 def related_objects(target):
-	collector = Collector(None)
-	collector.add([target])
+    collector = Collector(None)
+    collector.add([target])
 
-	yield from collector.instances_with_model()
+    yield from collector.instances_with_model()
