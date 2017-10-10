0af1b4b2c3ae6da65c814275ff506becada95f71
Renato Neto <30633715+neratneto@users.noreply.github.com>
2017-10-10 11:15:54 -0300

    Fixing flake8 indentation problem

--- powerlibs/django/contrib/related_objects.py
+++ powerlibs/django/contrib/related_objects.py
@@ -7,33 +7,33 @@ from django.views.generic import View
 
 
 def related_objects(target):
-	collector = Collector(None)
-	collector.add([target])
+        collector = Collector(None)
+        collector.add([target])
 
-	yield from collector.instances_with_model()
+        yield from collector.instances_with_model()
 
 
 class RelatedObjectsModelMixin(View, models.Model):
-	"""
-	To use it this mixin, make a http OPTIONS request to the location you want to analyze.
-	"""
+        """
+        To use it this mixin, make a http OPTIONS request to the location you want to analyze.
+        """
 
-	class Meta:
-		abstract = True
+        class Meta:
+            abstract = True
 
-	allowed_methods = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+        allowed_methods = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 
-	def options(self, request, *args, **kwargs):
-		if 'OPTIONS' not in self.allowed_methods:
-			return Http400('Method Not Allowed')
+        def options(self, request, *args, **kwargs):
+            if 'OPTIONS' not in self.allowed_methods:
+                return Http400('Method Not Allowed')
 
-		related = []
-		for model, obj in related_objects(DetailEndpoint.get_instance(self, request, *args, **kwargs)):
-			related.append(obj)
-			related.append(model)
+            related = []
+            for model, obj in related_objects(DetailEndpoint.get_instance(self, request, *args, **kwargs)):
+                related.append(obj)
+                related.append(model)
 
-		response = HttpResponse()
-		response['Allow'] = self.allowed_methods
-		response['Related-Objects'] = related
+            response = HttpResponse()
+            response['Allow'] = self.allowed_methods
+            response['Related-Objects'] = related
 
-		return response
+            return response
