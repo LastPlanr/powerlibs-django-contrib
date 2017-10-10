a6ba6d61c38b7df4e61e2fae4600e8de6c7c7b46
Renato Neto <30633715+neratneto@users.noreply.github.com>
2017-10-10 10:34:54 -0300

    Added a Mixin
    Creation of the RelatedObjectsModelMixin, which allows the user to use the related_objects function through a http OPTIONS request.

--- powerlibs/django/contrib/related_objects.py
+++ powerlibs/django/contrib/related_objects.py
@@ -1,8 +1,39 @@
+from django.db import models
 from django.db.models.deletion import Collector
+from django.http import HttpResponse
+from powerlibs.django.restless.modelviews import DetailEndpoint
+from powerlibs.django.restless.http import Http400
+from django.views.generic import View
 
 
 def related_objects(target):
-    collector = Collector(None)
-    collector.add([target])
+	collector = Collector(None)
+	collector.add([target])
 
-    yield from collector.instances_with_model()
+	yield from collector.instances_with_model()
+
+
+class RelatedObjectsModelMixin(View, models.Model):
+	"""
+	To use it this mixin, make a http OPTIONS request to the location you want to analyze.
+	"""
+
+	class Meta:
+		abstract = True
+
+	allowed_methods = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+
+	def options(self, request, *args, **kwargs):
+		if 'OPTIONS' not in self.allowed_methods:
+			return Http400('Method Not Allowed')
+
+		related = []
+		for model, obj in related_objects(DetailEndpoint.get_instance(self, request, *args, **kwargs)):
+			related.append(obj)
+			related.append(model)
+
+		response = HttpResponse()
+		response['Allow'] = self.allowed_methods
+		response['Related-Objects'] = related
+
+		return response
